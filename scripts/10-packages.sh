7cc5ee538f20b0d267e1131d9c8f465d3f5ea7f2
Valter Nordh <valter.nordh@gmail.com>
2026-08-04 19:49:12 +0200

    Install baseline packages

--- scripts/10-packages.sh
+++ scripts/10-packages.sh
@@ -2,4 +2,26 @@
 
 set -Eeuo pipefail
 
-log_info "Package installation (not implemented yet)"
+require_root
+
+packages=(
+    bash-completion
+    ca-certificates
+    curl
+    fzf
+    git
+    jq
+    nano
+    rsync
+    shellcheck
+    tmux
+    tree
+)
+
+log_info "Updating package index"
+apt-get update
+
+log_info "Installing baseline packages"
+apt-get install -y "${packages[@]}"
+
+log_info "Baseline packages installed"
