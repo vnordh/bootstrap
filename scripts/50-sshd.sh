a3a1af29c586e3b22f47e0e008e33038b5624f67
Valter Nordh <valter.nordh@gmail.com>
2026-08-05 11:49:57 +0200

    Update

--- scripts/50-sshd.sh
+++ scripts/50-sshd.sh
@@ -2,7 +2,7 @@
 
 set -Eeuo pipefail
 
-source_file="${SCRIPT_DIR}/config/sshd/20-bootstrap.conf"
+source_file="${SCRIPT_DIR}/config/sshd/sshd.conf"
 target_file="/etc/ssh/sshd_config.d/20-bootstrap.conf"
 backup_file="${target_file}.previous"
 
@@ -35,27 +35,6 @@ else
 
     rm -f "$backup_file"
 fi
-
-if [[ -f "$target_file" ]]; then
-    cp -p "$target_file" "$backup_file"
-fi
-
-install -o root -g root -m 0644 "$source_file" "$target_file"
-
-if ! sshd -t; then
-    log_error "Invalid SSH configuration; restoring previous state"
-
-    if [[ -f "$backup_file" ]]; then
-        mv "$backup_file" "$target_file"
-    else
-        rm -f "$target_file"
-    fi
-
-    sshd -t || die "SSH configuration remains invalid after rollback"
-    die "SSH configuration was not installed"
-fi
-
-rm -f "$backup_file"
 
 if systemctl list-unit-files --type=socket --no-legend |
     awk '{print $1}' |
